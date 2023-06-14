a0891a03430026800181fe26fd5d47ee1a0e4ca4
Davi Borges <davi.borges@accenture.com>
2023-06-14 12:43:16 -0400

    new fix to step 3

--- info_tooling_step3.js
+++ info_tooling_step3.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
-const jsforce = require('jsforce');
-const fs = require('fs');
-const ProgressBar = require('progress');
-const path = require('path');
-const csv = require('csv-parser');  // csv-parser library
+require("dotenv").config();
+const jsforce = require("jsforce");
+const fs = require("fs/promises");
+const ProgressBar = require("progress");
+const path = require("path");
+const csv = require("csv-parser"); // csv-parser library
 
 // Define your Salesforce credentials
 const username = process.env.SFDC_USERNAME;
@@ -12,84 +12,89 @@ const loginUrl = process.env.SFDC_LOGINURL;
 
 // Create a connection to Salesforce
 const conn = new jsforce.Connection({
-  loginUrl : loginUrl,
-  version : '57.0'  // Set your desired API version here
+  loginUrl: loginUrl,
+  version: "57.0", // Set your desired API version here
 });
 
-function appendCSVLine(file, data) {
-  file.write(Object.values(data).map(value => `"${value}"`).join(',') + '\n');
+async function appendCSVLine(file, data) {
+  await fs.appendFile(
+    file,
+    Object.values(data)
+      .map((value) => `"${value}"`)
+      .join(",") + "\n"
+  );
 }
 
 async function main() {
   try {
     try {
-        await conn.login(username, password);
+      await conn.login(username, password);
     } catch (e) {
-        fs.appendFileSync('dependencies_error.csv', `Error logging in: ${e.message}\n`);
-        throw e;  // rethrow the error after logging
+      await fs.appendFile(
+        "dependencies_error.csv",
+        `Error logging in: ${e.message}\n`
+      );
+      throw e; // rethrow the error after logging
     }
 
     // Get a list of CSV files in 'sobjects' subfolder
-    const files = fs.readdirSync('sobjects').filter(file => file.endsWith('.csv'));
+    const files = await fs
+      .readdir("sobjects")
+      .filter((file) => file.endsWith(".csv"));
 
-    const bar = new ProgressBar(':bar :current/:total SObjects processed', { total: files.length });
-
-    // Create dependencies CSV and error log CSV
-    const dependenciesFile = fs.createWriteStream('dependencies.csv');
-    const errorsFile = fs.createWriteStream('dependencies_error.csv');
+    const bar = new ProgressBar(":bar :current/:total SObjects processed", {
+      total: files.length,
+    });
 
     // Write headers
-    appendCSVLine(dependenciesFile, {
-      MetadataComponentId: 'MetadataComponentId',
-      MetadataComponentName: 'MetadataComponentName',
-      MetadataComponentType: 'MetadataComponentType',
-      RefMetadataComponentId: 'RefMetadataComponentId',
-      RefMetadataComponentName: 'RefMetadataComponentName',
-      RefMetadataComponentType: 'RefMetadataComponentType'
+    await appendCSVLine("dependencies.csv", {
+      MetadataComponentId: "MetadataComponentId",
+      MetadataComponentName: "MetadataComponentName",
+      MetadataComponentType: "MetadataComponentType",
+      RefMetadataComponentId: "RefMetadataComponentId",
+      RefMetadataComponentName: "RefMetadataComponentName",
+      RefMetadataComponentType: "RefMetadataComponentType",
     });
-    appendCSVLine(errorsFile, {
-      SObjectName: 'SObjectName',
-      Id: 'Id',
-      Error: 'Error'
-    });
-
+   
     for (let file of files) {
-      const sobjectName = path.basename(file, '.csv');
+      const sobjectName = path.basename(file, ".csv");
 
       const records = [];
-      fs.createReadStream(path.join('sobjects', file))
+      fs.createReadStream(path.join("sobjects", file))
         .pipe(csv())
-        .on('data', row => records.push(row))
-        .on('end', async () => {
+        .on("data", (row) => records.push(row))
+        .on("end", async () => {
           console.log(`Read ${records.length} rows from ${file}`);
 
           for (let record of records) {
             // Ignore Id "000000000000000AAA"
-            if (record.Id !== '000000000000000AAA') {
+            if (record.Id !== "000000000000000AAA") {
               let retried = false;
 
               const getDependencies = async () => {
                 try {
                   // Query the metadata component dependencies for this Id
-                  const dependencies = await conn.tooling.query(`SELECT MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMetadataComponentId, RefMetadataComponentName, RefMetadataComponentType FROM MetadataComponentDependency WHERE MetadataComponentId = '${record.Id}'`);
+                  const dependencies = await conn.tooling.query(
+                    `SELECT MetadataComponentId, MetadataComponentName, MetadataComponentType, RefMetadataComponentId, RefMetadataComponentName, RefMetadataComponentType FROM MetadataComponentDependency WHERE MetadataComponentId = '${record.Id}'`
+                  );
 
-                  dependencies.records.forEach(dependency => {
-                    console.log(JSON.stringify(dependency));
-                    appendCSVLine(dependenciesFile, dependency);
-                  });
+                  for (let dependency of dependencies.records) {
+                    delete dependency.attributes; // Remove the attributes element
+                    await appendCSVLine("dependencies.csv", dependency);
+                  }
                 } catch (e) {
                   // If it was a timeout error and we haven't retried yet
-                  if (e.message.includes('timeout') && !retried) {
+                  if (e.message.includes("timeout") && !retried) {
                     retried = true;
 
                     // Wait 10 seconds and retry
                     setTimeout(getDependencies, 10000);
                   } else {
                     // Log error
-                    appendCSVLine(errorsFile, {
-                      'SObjectName': sobjectName,
-                      'Id': record.Id,
-                      'Error': e.message
+                    await appendCSVLine("dependencies_error.csv", {
+                      SObjectName: sobjectName,
+                      Id: record.Id,
+                      Error: e.message,
                     });
                   }
                 }
@@ -102,11 +107,8 @@ async function main() {
           bar.tick();
         });
     }
-
-    dependenciesFile.end();
-    errorsFile.end();
   } catch (e) {
-    console.error('Error:', e.message);
+    console.error("Error:", e.message);
   }
 }
 
